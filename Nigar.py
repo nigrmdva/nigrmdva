03d915cfd38ffd174c44c1ebe203bace65583567
nigrmdva <151825547+nigrmdva@users.noreply.github.com>
2023-12-07 12:26:35 +0400

    Add files via upload

---
+++ Nigar.py
@@ -0,0 +1,30 @@
+import random
+def printList(lst):
+    for i in range(len(lst)):
+        for j in range(len(lst[i])):
+            print(lst[i][j],end='    ')
+        print()
+
+p=1
+n=4
+m=4
+a=[]
+for i in range(n):
+    a.append([])
+    for j in range(m):
+        a[i].append(random.randint(2,6))
+printList(a)
+print()
+for i in range(n):
+    p=0
+    if (i) % 2==0:
+        for j in range(m):
+                p=p+(a[i][j]**2)
+        print(i+1,"-ci setrdeki elementlerin hasili:", p)
+    else:
+        print("\n")
+
+
+
+
+
